6bd5959049c9072f7bb215966007520a382038b8
Lalit Solanki <55562345+imsolanki@users.noreply.github.com>
2024-03-22 17:45:02 +0530

    Time: 24 ms (8.63%) | Memory: 44.6 MB (35.70%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,7 +6,7 @@ class Solution {
         HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             if (map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+                map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0) + 1);
             } else {
                 map.put(s.charAt(i), 1);
             }
@@ -15,7 +15,7 @@ class Solution {
         System.out.println(map);
         for (int i = 0; i < s.length(); i++) {
             if (map.containsKey(t.charAt(i))) {
-                map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+                map.put(t.charAt(i), map.getOrDefault(t.charAt(i),0) - 1);
             }
 
         }
